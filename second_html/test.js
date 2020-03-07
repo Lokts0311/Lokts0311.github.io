4391dc595f19170695f87418ea38edcfa4283f28
Lokts0311 <49160613+Lokts0311@users.noreply.github.com>
2020-03-07 18:49:21 +0800

    Update test.js

--- second_html/test.js
+++ second_html/test.js
@@ -1,11 +1,11 @@
 window.onload = function()
 {
+  alert(document.getElementById("yumi"));
   document.getElementById("yumi").onclick = changephoto;
 };
 
 function changephoto()
 {
-    alert(document.getElementById("yumi"));
     if(document.getElementById("yumi").src == ( "yumi.jpg" ))
     {
       document.getElementById("yumi").src = "yumi1.jpg";
